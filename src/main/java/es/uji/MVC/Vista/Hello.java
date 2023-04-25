8ef90be6b14eafee49c131727ed4024d365e39ed
al415696 <95293214+al415696@users.noreply.github.com>
2023-04-25 12:32:22 +0200

    Final sesion1

--- src/main/java/es/uji/MVC/Vista/Hello.java
+++ src/main/java/es/uji/MVC/Vista/Hello.java
@@ -1,11 +1,12 @@
 package es.uji.MVC.Vista;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -19,85 +20,43 @@ public class Hello extends Application {
         primaryStage.setTitle("Song Recommender");
         StackPane root = new StackPane();
 
-
-
-        //esto es una modificación muy imporante
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        ToggleGroup gRecommend = new ToggleGroup();
+        //num recomendaciones
+        Label numRecomendLabel = new Label("Number of recomendations");
+        Spinner<Double> numRecomendSpinner = new Spinner<>();
+        HBox hboxNumRecomend = new HBox(numRecomendLabel,numRecomendSpinner);
+        hboxNumRecomend.setSpacing(10);
+        hboxNumRecomend.setAlignment(Pos.CENTER_LEFT);
+
+        //Options
+            //Criteria
+        Label based = new Label("Recommend based on:");
+        ToggleGroup basedOnGroup = new ToggleGroup();
+        ToggleButton features = new ToggleButton("Song features");
+        features.setToggleGroup(basedOnGroup);
+        ToggleButton genre = new ToggleButton("Guessed genre");
+        genre.setToggleGroup(basedOnGroup);
+        VBox gessedBox = new VBox(based, features,genre);
+            //Distance
+
+
+        HBox hboxOptions = new HBox(gessedBox);
+        //General
+        Label uno = new Label("Uno");
+        Label dos = new Label("Dos");
+        Label tres = new Label("Tres");
+        Label cuatro = new Label("Cuatro");
+        VBox vBox = new VBox(hboxNumRecomend,hboxOptions, dos, tres, cuatro);
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.TOP_LEFT);
+        root.getChildren().add(vBox);
+        primaryStage.setScene(new Scene(root, 250, 250));
+        primaryStage.show();
+
+
+        //https://www.youtube.com/watch?v=VUVqamT8Npc
+
+
+        /*ToggleGroup gRecommend = new ToggleGroup();
         RadioButton knn = new RadioButton("Recommend based on song features");
         knn.setOnAction(e -> System.out.println("Seleccionada opción knn."));
         RadioButton kmeans = new RadioButton("Recommend based on guessed genre");
@@ -106,7 +65,8 @@ public class Hello extends Application {
         kmeans.setToggleGroup(gRecommend);
         root.getChildren().add(kmeans);
         root.getChildren().add(knn);
-        primaryStage.setScene(new Scene(root, 250, 250));
-        primaryStage.show();
+
+         */
+
     }
 }
