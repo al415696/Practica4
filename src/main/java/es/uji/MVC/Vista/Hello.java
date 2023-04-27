bff59a6b9ee63e898af474042f228aadc4232187
al415696 <95293214+al415696@users.noreply.github.com>
2023-04-27 14:20:05 +0200

    Versión Mejorada en casa1

--- src/main/java/es/uji/MVC/Vista/Hello.java
+++ src/main/java/es/uji/MVC/Vista/Hello.java
@@ -1,12 +1,15 @@
 package es.uji.MVC.Vista;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -20,36 +23,57 @@ public class Hello extends Application {
         primaryStage.setTitle("Song Recommender");
         StackPane root = new StackPane();
 
+        //Options
+        Label optionsLabel = new Label("Options");
+        optionsLabel.setFont(Font.font(17));
+
         //num recomendaciones
         Label numRecomendLabel = new Label("Number of recomendations");
+        numRecomendLabel.setFont(Font.font(13));
         Spinner<Double> numRecomendSpinner = new Spinner<>();
         HBox hboxNumRecomend = new HBox(numRecomendLabel,numRecomendSpinner);
         hboxNumRecomend.setSpacing(10);
         hboxNumRecomend.setAlignment(Pos.CENTER_LEFT);
 
-        //Options
+        //Horizontal Options Box
             //Criteria
-        Label based = new Label("Recommend based on:");
+        Label basedLabel = new Label("Recommend based on:");
+        basedLabel.setFont(Font.font(13));
         ToggleGroup basedOnGroup = new ToggleGroup();
         ToggleButton features = new ToggleButton("Song features");
         features.setToggleGroup(basedOnGroup);
         ToggleButton genre = new ToggleButton("Guessed genre");
         genre.setToggleGroup(basedOnGroup);
-        VBox gessedBox = new VBox(based, features,genre);
+        VBox basedBox = new VBox(basedLabel, features,genre);
             //Distance
-
-
-        HBox hboxOptions = new HBox(gessedBox);
+        Label distanceLabel = new Label("Recommend based on:");
+        distanceLabel.setFont(Font.font(13));
+        ToggleGroup distanceGroup = new ToggleGroup();
+        ToggleButton manhathanButton = new ToggleButton("High precicion");
+        manhathanButton.setToggleGroup(distanceGroup);
+        ToggleButton euclideanButton = new ToggleButton("Standard precision");
+        euclideanButton.setToggleGroup(distanceGroup);
+        VBox distanceBox = new VBox(distanceLabel, manhathanButton,euclideanButton);
+            //Options box
+        HBox hboxOptions = new HBox(basedBox,distanceBox);
+        hboxOptions.setSpacing(30);
+        //Songs
+        Label songsLabel = new Label("Song Titles");
+        songsLabel.setFont(Font.font(17));
+            //Song list
+        ObservableList<String> songListNames = FXCollections.observableArrayList("Rightfully", "Peer Gynt", "Crazy My Beat", "Diggy Hole", "Bad Apple", "Star Platinum" +
+                "Armor-clad Faith", "Nights Of Fire", "Deja Vu", "Rumbling", "The Day", "Flowering Night");
+        ListView songList = new ListView<>(songListNames);
         //General
         Label uno = new Label("Uno");
         Label dos = new Label("Dos");
         Label tres = new Label("Tres");
         Label cuatro = new Label("Cuatro");
-        VBox vBox = new VBox(hboxNumRecomend,hboxOptions, dos, tres, cuatro);
+        VBox vBox = new VBox(optionsLabel,hboxNumRecomend,hboxOptions,songsLabel,songList,uno, dos, tres, cuatro);
         vBox.setSpacing(10);
         vBox.setAlignment(Pos.TOP_LEFT);
         root.getChildren().add(vBox);
-        primaryStage.setScene(new Scene(root, 250, 250));
+        primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
 
 
